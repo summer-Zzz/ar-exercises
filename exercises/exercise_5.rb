ec4b9165109ba3c85eaaa11486ef4f2addd4c972
Hongjin Zhu <summerellaooo@gmail.com>
2021-04-27 14:44:16 +0000

    update excercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -11,6 +11,8 @@ puts "----------"
 
 total_revenue = Store.sum(:annual_revenue)
 average_revenue = Store.average(:annual_revenue)
+rich_stores_number = Store.where("annual_revenue > ?", 1000000).count
 
 puts total_revenue
 puts average_revenue
+puts rich_stores_number
